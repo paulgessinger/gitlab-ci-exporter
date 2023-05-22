3e40a5b7fbb58be8b78f3ad8ed010b2a99db124b
Paul Gessinger <hello@paulgessinger.com>
2023-05-22 16:07:05 +0200

    relax pyver, config

--- src/gitlab_ci_exporter/update.py
+++ src/gitlab_ci_exporter/update.py
@@ -4,7 +4,7 @@ from prometheus_client import Gauge
 gitlab_ci_job_count = Gauge(
     name="gitlab_ci_job_count",
     documentation="The total number of jobs running for various categories",
-    labelnames=["status"],
+    labelnames=["status", "job_name"],
 )
 
 
@@ -25,4 +25,4 @@ class Updater:
 
         gitlab_ci_job_count.clear()
         for job in jobs:
-            gitlab_ci_job_count.labels(status=job.status).inc()
+            gitlab_ci_job_count.labels(status=job.status, job_name=job.name).inc()
