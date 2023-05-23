1db269b4b72158461db62fa339e4882468a8170b
Paul Gessinger <hello@paulgessinger.com>
2023-05-23 14:55:10 +0200

    add db, update jobs

--- src/gitlab_ci_exporter/cli.py
+++ src/gitlab_ci_exporter/cli.py
@@ -1,8 +1,14 @@
+from pathlib import Path
+import tempfile
+
 import typer
-from prometheus_client import start_http_server
+from prometheus_client import generate_latest, start_http_server
 from apscheduler.schedulers.blocking import BlockingScheduler
 
+from gitlab_ci_exporter.db import prepare_database
+
 from .update import Updater
+from .db import database
 
 cli = typer.Typer()
 
@@ -15,26 +21,36 @@ def serve(
     interval: int = typer.Option(30, envvar="GLE_INTERVAL"),
     port: int = typer.Option(8000, envvar="PORT"),
 ):
+    with tempfile.NamedTemporaryFile() as t:
+        prepare_database(Path(t.name))
+        updater = Updater(host=host, token=token)
+        start_http_server(port)
 
-    updater = Updater(host=host, token=token)
-    #  updater.tick(project)
-    #  return
-    start_http_server(port)
+        scheduler = BlockingScheduler()
+        scheduler.add_executor("threadpool")
 
-    scheduler = BlockingScheduler()
-    scheduler.add_executor("threadpool")
+        scheduler.add_job(
+            updater.tick, "interval", seconds=interval, kwargs={"project": project}
+        )
 
-    scheduler.add_job(
-        updater.tick, "interval", seconds=interval, kwargs={"project": project}
-    )
-
-    try:
-        print("Starting scheduler")
-        scheduler.start()
-    except (KeyboardInterrupt, SystemExit):
-        pass
+        try:
+            print("Starting scheduler")
+            scheduler.start()
+        except (KeyboardInterrupt, SystemExit):
+            pass
 
 
 @cli.command()
-def tick():
-    tick()
+def tick(
+    host: str = typer.Option(..., envvar="GLE_HOST"),
+    token: str = typer.Option(..., envvar="GLE_TOKEN"),
+    project: str = typer.Option(..., envvar="GLE_PROJECT"),
+    interval: int = typer.Option(30, envvar="GLE_INTERVAL"),
+    port: int = typer.Option(8000, envvar="PORT"),
+):
+    with tempfile.NamedTemporaryFile() as t:
+        prepare_database(Path(t.name))
+        updater = Updater(host=host, token=token)
+        updater.tick(project)
+
+    print(generate_latest().decode())
---
+++ src/gitlab_ci_exporter/db.py
@@ -0,0 +1,29 @@
+from pathlib import Path
+
+import peewee
+
+database = peewee.SqliteDatabase(None)
+
+def prepare_database(dbfile: Path):
+    database.init(dbfile)
+    database.connect()
+    database.create_tables([Job])
+
+class Model(peewee.Model):
+    class Meta:
+        database = database
+
+class Job(Model):
+    id = peewee.IntegerField(primary_key=True, unique=True)
+    commit_sha = peewee.CharField()
+    created_at = peewee.DateTimeField()
+    started_at = peewee.DateTimeField(null=True)
+    finished_at = peewee.DateTimeField(null=True, index=True)
+    duration = peewee.FloatField(null=True)
+    queued_duration = peewee.FloatField(null=True)
+    name = peewee.CharField()
+    ref = peewee.CharField()
+    status = peewee.CharField()
+    failure_reason = peewee.CharField(null=True)
+
+
